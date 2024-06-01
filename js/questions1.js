e4ecdaf104b76f6f80ed66087a5fd7e3121a4004
Graph3ned <edbertetchon2@gmail.com>
2024-06-02 00:18:09 +0800

    Add files via upload

---
+++ js/questions1.js
@@ -0,0 +1,168 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "John owes his friend $30. He borrows another $25 from the same friend and then repays $15. How much does he still owe his friend?",
+  answer: "a) $40",
+  options: [
+    "a) $40",
+    "b) $55",
+    "c) $25",
+    "d) $20"
+  ]
+},
+  {
+  numb: 2,
+  question: "What is the product of −3 and −4?",
+  answer: "b) 12",
+  options: [
+    "a) −12",
+    "b) 12",
+    "c) 7",
+    "d) −7"
+  ]
+},
+  {
+  numb: 3,
+  question: "An elevator is on the 10th floor. It goes down 7 floors, then up 4 floors, and finally down 2 floors. What floor is the elevator on now?",
+  answer: "d) 6th floor",
+  options: [
+    "a) 5th floor",
+    "b) 3rd floor",
+    "c) 7th floor",
+    "d) 6th floor"
+  ]
+},
+  {
+  numb: 4,
+  question: "What is the result of dividing −16 by 4?",
+  answer: "a) −4",
+  options: [
+    "a) −4",
+    "b) 4",
+    "c) −12",
+    "d) 12"
+  ]
+},
+  {
+  numb: 5,
+  question: "Simplify: 6×(−5)",
+  answer: "a) −30",
+  options: [
+    "a) −30",
+    "b) 30",
+    "c) −1",
+    "d) 1"
+  ]
+},
+  {
+  numb: 6,
+  question: "Maria is tracking the temperature change over four days. The temperature changes were: +5°C, -3°C, -7°C, and +2°C. If the initial temperature was 10°C, what is the temperature at the end of the fourth day?",
+  answer: "d) 8°C",
+  options: [
+    "a) 7°C",
+    "b) 12°C",
+    "c) 10°C",
+    "d) 8°C"
+  ]
+},
+  {
+  numb: 7,
+  question: "What is 18÷(−3)?",
+  answer: "a) −6",
+  options: [
+    "a) −6",
+    "b) 6",
+    "c) −15",
+    "d) 15"
+  ]
+},
+  {
+  numb: 8,
+  question: "What is the result of (−12)−(−7)?",
+  answer: "c) −5",
+  options: [
+    "a) −19",
+    "b) 19",
+    "c) −5",
+    "d) 5"
+  ]
+},
+  {
+  numb: 9,
+  question: "Calculate: (−4)×(−6)",
+  answer: "b) 24",
+  options: [
+    "a) −24",
+    "b) 24",
+    "c) −10",
+    "d) 10"
+  ]
+},
+  {
+  numb: 10,
+  question: "If x=−7 and y=3, what is x * y?",
+  answer: "a) −21",
+  options: [
+    "a) −21",
+    "b) 21",
+    "c) −10",
+    "d) 10"
+  ]
+},
+  {
+  numb: 11,
+  question: "If we are going to add -58 to 61, what will be the answer?",
+  answer: "c) 3",
+  options: [
+    "a) 119",
+    "b) -3",
+    "c) 3",
+    "d) -119"
+  ]
+},
+  {
+  numb: 12,
+  question: "Which of the following equations is true?",
+  answer: "b) 12 x 4 = 48",
+  options: [
+    "a) -12 – 7 = -5",
+    "b) 12 x 4 = 48",
+    "c) -13 x 5 = 65",
+    "d) none of this"
+  ]
+},
+  {
+  numb: 13,
+  question: "Which do not belong to the group?",
+  answer: "a) Divition of Integers",
+  options: [
+    "a) Divition of Integers",
+    "b) Subtraction of Integers",
+    "c) Multiplication of Integers",
+    "d) Addition of Integers"
+  ]
+},
+  {
+  numb: 14,
+  question: "Which of the following is not true?",
+  answer: "d) none of this",
+  options: [
+    "a) -24(3) = -72",
+    "b) -12 +(-5) = -17",
+    "c) -17 – (6) = -23",
+    "d) none of this"
+  ]
+},
+  {
+  numb: 15,
+  question: "Which of the following statements is true?",
+  answer: "c) In Division of Integers if the equation has unlike sign, the sign of the answer is always negative.",
+  options: [
+    "a) In Addition of Integers if the equation has unlike sign we subtract the larger number from the smaller number.",
+    "b) In Multiplication of Integers if the equation has unlike sign, the sign of the answer are always positive.",
+    "c) In Division of Integers if the equation has unlike sign, the sign of the answer is always negative.",
+    "d) The Four Operations of Integers are the following; Addition of Integers, Substraction of Integers, Multiplication of Integers, and Division of Integers."
+  ]
+}
+];
